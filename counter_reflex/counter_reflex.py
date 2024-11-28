5c3b054802902244f94b532a593e4b5d158259d7
María García <mariav2601@gmail.com>
2024-11-27 22:59:47 -0400

    creamos las funciones para incrementar y decrementar el contador

--- counter_reflex/counter_reflex.py
+++ counter_reflex/counter_reflex.py
@@ -6,6 +6,12 @@ from rxconfig import config
 class State(rx.State):
     count: int = 0
 
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
 
 
 app = rx.App()
