9b252fbf2b7202904e7b6dc5c44d32b1b857b821
María García <mariav2601@gmail.com>
2024-11-27 23:16:08 -0400

    update

--- counter_reflex/counter_reflex.py
+++ counter_reflex/counter_reflex.py
@@ -11,19 +11,23 @@ class State(rx.State):
         self.count -= 1
 
 def index():
-    return rx.hstack(
-        rx.button(
-            "Decrement",
-            color_scheme="ruby",
-            on_click=State.decrement,
-        ),
-        rx.heading(State.count, font_size="2em"),
-        rx.button(
-            "Increment",
-            color_scheme="grass",
-            on_click=State.increment,
-        ),
-        spacing="4",
+    return rx.flex(
+        rx.center(
+            rx.hstack(
+                rx.button(
+                    "Decrement",
+                    color_scheme="ruby",
+                    on_click=State.decrement,
+                ),
+                rx.heading(State.count, font_size="2em"),
+                rx.button(
+                    "Increment",
+                    color_scheme="grass",
+                    on_click=State.increment,
+                ),
+            spacing="4",
+            )
+        )
     )
 
 
